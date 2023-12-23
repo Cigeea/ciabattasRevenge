28077b80b7ce02b77328b9babc74e5d2a98a5411
Cigeea <aeegic@live.fr>
2023-12-23 12:19:22 +0100

    Video 4 + 5 adding background and rendering sprites

--- src/components/level-layout/RenderLevel.js
+++ src/components/level-layout/RenderLevel.js
@@ -1,30 +1,39 @@
 import styles from "./RenderLevel.module.css";
 import Sprite from "../object-graphics/Sprite";
-import { CELL_SIZE } from "../../helpers/consts";
+import {
+  CELL_SIZE,
+  LEVEL_THEMES,
+  THEME_BACKGROUNDS,
+} from "../../helpers/consts";
+import LevelBackgroundTilesLayer from "./LevelBackgroundTilesLayer";
 
 export default function RenderLevel({ spriteSheetImage }) {
   const level = {
+    theme: LEVEL_THEMES.BLUE,
+    tilesWidth: 8,
+    tilesHeight: 8,
+
     placements: [
       // "Level 0"
-      // { id: 0, x: 0, y: 0, frameCoord: "0x2" },
-      // { id: 1, x: 1, y: 1, frameCoord: "0x2" },
-      // { id: 2, x: 2, y: 2, frameCoord: "0x2" },
-      // { id: 3, x: 3, y: 3, frameCoord: "0x2" },
-      // { id: 4, x: 4, y: 4, frameCoord: "0x2" },
-      // { id: 5, x: 5, y: 5, frameCoord: "0x2" },
-      // { id: 6, x: 6, y: 6, frameCoord: "0x2" },
-      // { id: 7, x: 7, y: 7, frameCoord: "0x2" },
+      { id: 0, x: 0, y: 0, frameCoord: "0x2" },
+      { id: 1, x: 1, y: 1, frameCoord: "0x2" },
+      { id: 2, x: 2, y: 2, frameCoord: "0x2" },
+      { id: 3, x: 3, y: 3, frameCoord: "0x2" },
+      { id: 4, x: 4, y: 4, frameCoord: "0x2" },
+      { id: 5, x: 5, y: 5, frameCoord: "0x2" },
+      { id: 6, x: 6, y: 6, frameCoord: "0x2" },
+      { id: 7, x: 7, y: 7, frameCoord: "0x2" },
 
       // Level '1'
-      { id: 0, x: 0, y: 0, frameCoord: "0x2" },
-      { id: 1, x: 4, y: 2, frameCoord: "0x2" },
-      { id: 2, x: 2, y: 2, frameCoord: "0x2" },
-      { id: 3, x: 7, y: 3, frameCoord: "0x2" },
-      { id: 4, x: 2, y: 9, frameCoord: "0x2" },
-      { id: 5, x: 3, y: 5, frameCoord: "0x2" },
-      { id: 6, x: 1, y: 6, frameCoord: "0x2" },
-      { id: 7, x: 7, y: 7, frameCoord: "0x2" },
-      { id: 8, x: 9, y: 7, frameCoord: "0x2" },
+      // { id: 0, x: 0, y: 0, frameCoord: "0x2" },
+      // { id: 1, x: 4, y: 2, frameCoord: "0x2" },
+      // { id: 2, x: 2, y: 2, frameCoord: "0x2" },
+      // { id: 3, x: 7, y: 3, frameCoord: "0x2" },
+      // { id: 4, x: 2, y: 9, frameCoord: "0x2" },
+      // { id: 5, x: 3, y: 5, frameCoord: "0x2" },
+      // { id: 6, x: 1, y: 6, frameCoord: "0x2" },
+      // { id: 7, x: 7, y: 7, frameCoord: "0x2" },
+      // { id: 8, x: 9, y: 7, frameCoord: "0x2" },
 
       //Level '2'
       // { id: 0, x: 2, y: 2, frameCoord: "0x2" },
@@ -48,8 +57,14 @@ export default function RenderLevel({ spriteSheetImage }) {
   };
 
   return (
-    <div className={styles.fullScreenContainer}>
+    <div
+      className={styles.fullScreenContainer}
+      style={{
+        background: THEME_BACKGROUNDS[level.theme],
+      }}
+    >
       <div className={styles.gameScreen}>
+        <LevelBackgroundTilesLayer level={level} image={spriteSheetImage} />
         {level.placements.map((placement) => {
           // Wrap each Sprite in a positioned div
           const x = placement.x * CELL_SIZE + "px";
